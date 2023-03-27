359fe9b03647308e3118f5c36a43b2959f364983
xxx <xxx@web.de>
2023-03-27 21:56:57 +0200

    commit02

--- src/pz/CResourceReader.java
+++ src/pz/CResourceReader.java
@@ -1,26 +1,32 @@
 package pz;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.Properties;
 
 public class CResourceReader {
+
     public static void readPropertyFileInClassPath(String filename){
+
         try{
             InputStream inst = CResourceReader.class.getResourceAsStream(filename);
             Properties properties = new Properties();
             properties.load(inst);
             inst.close();
             String sLang = properties.getProperty("lang");
+            StringBuilder sb = new StringBuilder();
             Enumeration<?> enu = properties.propertyNames();
             while(enu.hasMoreElements()){
                 String key = (String) enu.nextElement();
-                System.out.println(key + ": " + properties.getProperty(key));
+                String s = String.format("%s: %s", key, properties.getProperty(key));
+                sb.append(s); sb.append("\n");
             }
+            JOptionPane.showMessageDialog(null,sb.toString(),"PROPERTIES",JOptionPane.INFORMATION_MESSAGE);
         }catch(Exception ex){ System.err.println(ex.getLocalizedMessage()); }
     }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String sProjectPath1 = System.getProperty("user.dir");
         String sProjectPath2 = new File("").getAbsolutePath();
         //file:/projectname/out/production/classname/packagename/
